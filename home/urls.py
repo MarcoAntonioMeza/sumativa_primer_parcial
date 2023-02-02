6dfdce2f2bc5e67a62b32917ad2d781e5646e6a9
Marco Antonio Meza <marcosmeza01@outlook.com>
2023-02-01 22:53:33 -0600

    usuarios

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,9 @@ from . import views
 urlpatterns = [
     #Url para  el inicio de la aplicacion inicio
     #path('', views.login_usuario, name='login'),
-    path('', views.login_usurio, name='login_usuario'),
+    path('iniciar-sesion', views.login_usurio, name='login_usuario'),
     path('registrar', views.registar_usuario, name='registar_usuario'),
+    path('cerrar-sesion/', views.cerrar_sesion, name='cerrar_sesion'),
+    path('home/', views.home, name='home'),
+    path('usuarios/', views.usuarios, name='usuarios'),
 ]
--- home/views.py
+++ home/views.py
@@ -4,7 +4,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.hashers import make_password
 from django.http.response import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
-from user.forms import LoginUsuario
+from user.forms import *
+from user.models import Usuario
 
 # Create your views here.
 
@@ -18,12 +19,49 @@ def login_usurio(request):
             user = authenticate(request, correo=correo, password=password)
             if user is not None:
                 login(request, user)
-                return render(request,'home/home.html')
+                return redirect('home')
             else:
-                return render(request,'usuarios/login.html',{'error ': "Correo  o Contrasena incorrecta"})
+                return render(request, 'usuarios/login.html')
 
-        #messages.error(request, 'Formulario Invalido')
-    return render(request,'usuarios/login.html')
+        # messages.error(request, 'Formulario Invalido')
+    return render(request, 'usuarios/login.html')
+
 
 def registar_usuario(request):
+    if request.POST:
+        signup_form = RegistrarUsuario(request.POST or None)
+        if signup_form.is_valid():
+            correo = signup_form.cleaned_data.get('correo')
+            nombre = signup_form.cleaned_data.get('nombre')
+            apellidos = signup_form.cleaned_data.get('apellidos')
+            password = signup_form.cleaned_data.get('password')
+            try:
+                user = get_user_model().objects.create(
+                    correo=correo,
+                    nombre=nombre,
+                    apellidos=apellidos,
+                    password=make_password(password),
+                    is_active=True
+                )
+                login(request, user)
+                return redirect('home')
+
+            except Exception as e:
+                print(e)
+                return JsonResponse({'detail': f'{e}'})
     return render(request,'usuarios/registrar.html')
+
+def cerrar_sesion(request):
+    logout(request)
+    return redirect('login_usuario')
+
+def home(request):
+    return render(request,'home/home.html')
+
+def usuarios(request):
+    usuarios = Usuario.objects.all()
+    #usuarios = list(Usuario.objects.values())
+    #return JsonResponse(usuarios)
+    return render(request,'usuarios/usuarios.html',{
+        'usuarios':usuarios
+    })

57cb41d28c56e68fdf8bffa45f203ab583d40695
Marco Antonio Meza <marcosmeza01@outlook.com>
2023-02-03 11:55:33 -0600

    Bug

--- core/settings.py
+++ core/settings.py
@@ -85,12 +85,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-''''default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-         
-    }'''
+''''
 
+        'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+         
+    }
+
+        '''
 DATABASES = {
     'default':  dj_database_url.config(        
         default='postgresql://postgres:postgres@localhost:5432/mysite',   

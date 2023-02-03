afe2e992b88c6f9ad19a6640d62169ab35a7d39e
Marco Antonio Meza <marcosmeza01@outlook.com>
2023-02-03 11:33:34 -0600

    Data Base

--- core/settings.py
+++ core/settings.py
@@ -85,13 +85,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    ''''default': {
+''''default': {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': BASE_DIR / 'db.sqlite3',
          
     }'''
+
+DATABASES = {
+    
     'default':  dj_database_url.config(        
         default='postgresql://postgres:postgres@localhost:5432/mysite',   
         conn_max_age=600   
